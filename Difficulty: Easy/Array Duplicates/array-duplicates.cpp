206d65d67deadd820a63b986bbde8381d3afd261
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-10-23 11:08:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.cpp
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -28,10 +28,12 @@ class Solution {
         //     }
         // }
         
+        // M3
         sort(arr.begin(), arr.end());
         for(int i = 0; i < arr.size() - 1; i++) {
             if(arr[i] == arr[i + 1]) {
                 ans.push_back(arr[i]);
+                // Skip all consecutive duplicates of the same number
                 while(i < arr.size() && arr[i] == arr[i + 1]) {
                     i++;
                 }
