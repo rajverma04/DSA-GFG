5bf599fa1bf0dc3c9c5e578a484a3c0739dbd046
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-19 18:35:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -4,17 +4,17 @@ class Solution {
         stack<char> st;
         
         for(auto ch : s) {
-            if(ch == '(' || ch == '{' || ch == '[') {
+            if(ch == '(' || ch == '{' || ch == '[') {       // if open braces come the push to stack
                 st.push(ch);
             } else {
-                if(st.empty()) {
+                if(st.empty()) {        // stack empty means no braces present
                     return false;
                 }
                 
-                char top = st.top();
-                st.pop();
+                char top = st.top();        // get top element
+                st.pop();       // delete the top element
                 
-                if((ch == ')' && top != '(') ||
+                if((ch == ')' && top != '(') ||     // if closing braces of top element is not same type which is top return false
                     (ch == '}' && top != '{') || 
                     (ch == ']' && top != '[')) {
                     return false;
@@ -22,7 +22,7 @@ class Solution {
             }
         }
         
-        return st.empty();
+        return st.empty();  // if all elements matched its pair means stack is empty
         
     }
 };
