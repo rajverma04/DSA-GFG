ed395c0bd17a6c85c2cdcf18a4bc1df13ae78f46
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-22 21:43:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     int missingNum(vector<int>& arr) {
-        // code here
+        // M1: sum of n natural number
         long long n = (long long)arr.size() + 1;         // actual size if number is not missing 
         long long sum = n * (n + 1) / 2;      // sum of n natural number
         long long arrSum = 0;                 // given array sum
@@ -11,5 +11,7 @@ class Solution {
         }
         
         return (int)(sum - arrSum);          // difference btw actual and given sum is the missing number
+        
+       
     }
 };
