30e71b53459b0097d8a0b994cd49480f4daad50e
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-12-07 23:37:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.cpp
@@ -0,0 +1,41 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int height(Node *root, bool &ans) {
+        if(!root) {
+            return 0;
+        }
+        
+        int left = height(root->left, ans);
+        int right = height(root->right, ans);
+        
+        if(abs(left - right) > 1) {
+            ans = 0;
+        }
+        
+        return 1 + max(left, right);
+    }
+    
+    
+    bool isBalanced(Node* root) {
+        bool ans = true;
+        
+        height(root, ans);
+        return ans;
+    }
+};
