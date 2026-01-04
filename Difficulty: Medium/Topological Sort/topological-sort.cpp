97bbec94684024eb661491d9cb20215e7dd1d343
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-04 16:33:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -14,30 +14,59 @@ class Solution {
     
     
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<int> ans;
-        vector<bool> visited(V, false);
-        
         // 🔥 STEP 1: Build adjacency list
         vector<vector<int>> adj(V);
         for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
         }
         
-        stack<int> st;
+        vector<int> ans;
+        // vector<bool> visited(V, false);
+        // stack<int> st;
         
+        // for(int i = 0; i < V; i++) {
+        //     if(!visited[i]) {
+        //         DFS(i, adj, st, visited);
+        //     }
+        // }
+        
+        
+        
+        // while(!st.empty()) {
+        //     ans.push_back(st.top());
+        //     st.pop();
+        // }
+        
+        
+        // M2: Kahn's Algo.
+        
+        vector<int> inDeg(V, 0);
         for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                DFS(i, adj, st, visited);
+            for(int j = 0; j < adj[i].size(); j++) {
+                inDeg[adj[i][j]]++;
             }
         }
         
-        
-        
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        // push all node of zero indegree into queue;
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(!inDeg[i]) {
+                q.push(i);
+            }
         }
         
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(int i = 0; i < adj[node].size(); i++) {
+                inDeg[adj[node][i]]--;
+                if(inDeg[adj[node][i]] == 0) {
+                    q.push(adj[node][i]);
+                }
+            }
+        }
         
         return ans;
     }
