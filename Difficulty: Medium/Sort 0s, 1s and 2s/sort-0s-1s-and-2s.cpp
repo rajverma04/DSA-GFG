9fbb6ad5b22d28fab0e1d74b8b3e0739c5a0fe39
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-05 21:05:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -1,27 +1,15 @@
 class Solution {
   public:
     void sort012(vector<int>& arr) {
-        // code here
-        int zerocount = 0, onecount = 0, twocount = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(auto it : arr) {
+            pq.push(it);
+        }
         
         for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] == 0) {
-                zerocount++;
-            } else if(arr[i] == 1) {
-                onecount++;
-            } else {
-                twocount++;
-            }
-        }
-        
-        for(int i = 0; i < zerocount; i++) {
-            arr[i] = 0;
-        }
-        for(int i = zerocount; i < zerocount + onecount; i++) {
-            arr[i] = 1;
-        }
-        for(int i = zerocount + onecount; i < arr.size(); i++) {
-            arr[i] = 2;
+            arr[i] = pq.top();
+            pq.pop();
         }
         
     }
