d457c243fe4dfb822463b8b16390b24693b304bc
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-26 16:14:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate Permutations of an array/generate-permutations-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> permuteDist(vector<int>& arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());
+
+        do {
+            ans.push_back(arr);
+        } while (next_permutation(arr.begin(), arr.end()));
+
+        return ans;
+    }
+};
