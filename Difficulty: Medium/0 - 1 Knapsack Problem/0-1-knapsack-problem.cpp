695f94c26db64a5058e22202c27c8569f339ed7f
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-01 02:48:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -2,30 +2,30 @@ class Solution {
   public:
     int find(int index, int W, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp) {
         // Base Condition
-        if(W == 0) {
-            return 0;
-        }
-        
         if(index == 0) {
+            if(wt[0] <= W) {
+                return val[0];
+            }
             return 0;
         }
         if(dp[index][W] != -1) {
             return dp[index][W];
         }
         
+        int notTake = 0 + find(index - 1, W, val, wt, dp);
+        int take = INT_MIN;
         
-        if(wt[index - 1] > W) {     // current weight > bag weight
-            return dp[index][W] = find(index - 1, W, val, wt, dp);
-        } else {    // current weight < bag weight : have two option take it or do not take it
-            return dp[index][W] = max(val[index - 1] + find(index - 1, W - wt[index - 1], val, wt, dp), find(index - 1, W, val, wt, dp));
+        if(wt[index] <= W) {     // current weight < bag weight
+            take = val[index] + find(index - 1, W - wt[index], val, wt, dp);
         }
         
+        return dp[index][W] = max(take, notTake);
         
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         int n = val.size(); // or wt.size() as both equal
         vector<vector<int>> dp(n + 1, vector<int> (W + 1, -1));
-        return find(n, W, val, wt, dp);
+        return find(n - 1, W, val, wt, dp);
         
     }
 };
