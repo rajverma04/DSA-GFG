c4749bc7e96dea8bb929fd852302c4cc43d7f04b
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-04 23:46:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,32 +1,22 @@
 class Solution {
-  public:
-    int find(int n, int m, string &s1, string &s2, int &ans, vector<vector<int>> &dp) {
-        if(n == 0 || m == 0) {
-            return 0;
-        }
-        if(dp[n][m] != -1) {
-            return dp[n][m];
-        }
-        
-        int len = 0;
-        if(s1[n - 1] == s2[m - 1]) {
-            len = 1 + find(n - 1, m - 1, s1, s2, ans, dp);
-            ans = max(ans, len);
-        }
-        find(n - 1, m, s1, s2, ans, dp);
-        find(n, m - 1, s1, s2, ans, dp);
-        
-        return dp[n][m] = len;
-    }
-    int longestCommonSubstr(string& s1, string& s2) {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        
+public:
+    int longCommSubstr(string& s1, string& s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
         int ans = 0;
-        find(n, m, s1, s2, ans, dp);
-        
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
         return ans;
-        
     }
 };
