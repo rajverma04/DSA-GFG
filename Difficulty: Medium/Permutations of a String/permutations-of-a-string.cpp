19a67ed09f2713000738a93d1a702d38b2d00191
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-23 09:52:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -3,41 +3,12 @@ class Solution {
     vector<string> findPermutation(string &s) {
         vector<string> ans;
         
-        if(s.empty()) {
-            return ans;
-        }
+        sort(s.begin(), s.end());
         
-        backtrack(s, 0, ans);
-        
-        // Remove duplicates if any and sort
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        do {
+            ans.push_back(s);
+        } while (next_permutation(s.begin(), s.end()));
         
         return ans;
-        
     }
-    
-    void backtrack(string &s, int start, vector<string> &ans) {
-        if(start == s.size()) {
-            ans.push_back(s);
-            return;
-        }
-        
-        // Set to track used characters at current position
-        unordered_set<char> used;
-        
-        for(int i = start; i < s.size(); i++) {
-            // Skip if character already used at this position (avoid duplicates)
-            if(used.find(s[i]) != used.end()) {
-                continue;
-            }
-            
-            used.insert(s[i]);
-            
-            swap(s[start], s[i]);
-            backtrack(s, start + 1, ans);
-            swap(s[start], s[i]);
-        }
-    }
-    
 };
