7ecdcc30aafbe0cf8585364937506e5fc7c47dcd
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-07-04 00:13:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -1,79 +1,70 @@
 /*
+Definition for Node
 class Node {
   public:
     int data;
     Node* left;
     Node* right;
 
-    // Constructor to initialize a new node
     Node(int val) {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = nullptr;
     }
 };
 */
 
+void leftsub(Node *root,vector<int>&ans)
+{   if(!root||(!root->left&&!root->right)) return;
+    ans.push_back(root->data);
+    if(root->left)
+    {
+        leftsub(root->left,ans);
+    }
+    else
+    {
+        leftsub(root->right,ans);
+    }
+    return;
+}
+void nonleaf(Node*root,vector<int>&ans)
+{   if(!root ) return;
+    if(!root->left&&!root->right)
+    {
+        ans.push_back(root->data);
+        return ;
+    }
+    
+    nonleaf(root->left,ans);
+    nonleaf(root->right,ans);
+    
+}
+void rightsub(Node *root,vector<int>&ans)
+{  if(!root||(!root->left&&!root->right)) return ;
+    if(root->right)
+    {
+            rightsub(root->right,ans);
+    }
+    else
+    {
+        rightsub(root->left,ans);
+    }
+    ans.push_back(root->data);
+    return;
+}
+
+
+
 class Solution {
   public:
-    // left subtree except leaf
-    void leftSubtree(Node *root, vector<int> &ans) {
-        if(!root || (!root->left && !root->right)) {
-            return;
-        }
-        
-        ans.push_back(root->data);
-        if(root->left) {
-            leftSubtree(root->left, ans);
-        } else {
-            leftSubtree(root->right, ans);
-        }
-        
-        return;
-    }
-    
-    // leaf node
-    void leafNode(Node *root, vector<int> &ans) {
-        if(!root) {
-            return;
-        }
-        if(!root->left && !root->right) {
-            ans.push_back(root->data);
-            return;
-        }
-        
-        leafNode(root->left, ans);
-        leafNode(root->right, ans);
-    }
-    
-    // reverse right sub tree except leaf
-    void rightSubtree(Node *root, vector<int> &ans) {
-        if(!root || (!root->left && !root->right)) {
-            return;
-        }
-        
-        if(root->right) {
-            rightSubtree(root->right, ans);
-        } else {
-            rightSubtree(root->left, ans);
-        }
-        
-        ans.push_back(root->data);
-        return;
-    }
-    
     vector<int> boundaryTraversal(Node *root) {
-        vector<int> ans;
+        vector<int>v;
+        v.push_back(root->data);
+        if(!root->left&&!root->right)
+        return v;
+        leftsub(root->left,v);
+        nonleaf(root,v);
+        rightsub(root->right,v);
+        return v;
         
-        ans.push_back(root->data);
-        
-        if(!root->left && !root->right) {
-            return ans;
-        }
-        leftSubtree(root->left, ans);
-        leafNode(root, ans);
-        rightSubtree(root->right, ans);
-        
-        return ans;
     }
 };
