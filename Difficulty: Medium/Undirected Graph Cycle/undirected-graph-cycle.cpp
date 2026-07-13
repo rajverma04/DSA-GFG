e39765b73afdc8fe56fdeac09cae3edb55f1d731
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-07-13 14:38:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,47 +1,44 @@
 class Solution {
   public:
-  
-    bool BFS(int node, vector<vector<int>>& adj, vector<bool> &visited) {
-        queue<pair<int, int>> q;
-        q.push({node, -1});
-        visited[node] = true;
-        while(!q.empty()) {
-            int child = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            
-            for(int i = 0; i < adj[child].size(); i++) {
-                if(visited[adj[child][i]] == false) {
-                    visited[adj[child][i]] = true;
-                    q.push({adj[child][i], child});
-                } else {
-                    if(adj[child][i] != parent) {
-                        return true;
-                    }
-                }
-            }
+    vector<int> parent, size;
+    int find(int node) {
+        if(parent[node] == node) {
+            return node;
         }
         
+        return parent[node] = find(parent[node]);
+    }
+    
+    bool unionSet(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if(pu == pv) {
+            return true;
+        }
+        if(size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
         return false;
     }
     
     bool isCycle(int V, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(V);
-        for(auto e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-        vector<bool> visited(V, false);
+        parent.resize(V);
+        size.resize(V, 1);
         
-        for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                bool ans = BFS(i, adj, visited);
-                if(ans) {
-                    return true;
-                }
+        iota(parent.begin(), parent.end(), 0);          // it will the vector from 0 to v - 1(sequentially)
+        
+        for(auto e : edges) {
+            if(!unionSet(e[0], e[1])) {
+                unionSet(e[0], e[1]);
+            } else {
+                return true;
             }
         }
         
         return false;
+        
     }
 };
