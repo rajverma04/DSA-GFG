41cefcad33ba58bfd9918f50464283812439bd9b
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-07-19 03:38:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        int count = 0;
+        
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                count++;
+                if(n / i != i) {
+                    count++;
+                }
+            }
+        }
+        
+        return (count == 2) ? true : false;
+        
+    }
+};
