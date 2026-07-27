ae7aa250b03ac32ad820874327e96ac07f6ac4f9
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-07-28 00:59:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     vector<vector<int>> dp;
-    int solve(int idx, int n, int c, vector<vector<int>>& mat) {
+    int solve(int idx, int c, vector<vector<int>>& mat) {
         if(idx == 0) {
             int maxi = 0;
             for(int i = 0; i < 3; i++) {
@@ -12,9 +12,7 @@ class Solution {
             
             return maxi;
         }
-        if(idx < 0) {
-            return 0;
-        }
+        
         if(dp[idx][c] != -1) {
             return dp[idx][c];
         }
@@ -22,7 +20,7 @@ class Solution {
         int maxi = 0;
         for(int i = 0; i < 3; i++) {
             if(i != c) {
-                maxi = max(maxi, mat[idx][i] + solve(idx - 1, n, i, mat));
+                maxi = max(maxi, mat[idx][i] + solve(idx - 1, i, mat));
             }
         }
         
@@ -31,8 +29,26 @@ class Solution {
     
     int maximumPoints(vector<vector<int>>& mat) {
         int n = mat.size();
-        dp.assign(n + 1, vector<int> (4, -1));
-        int c = -1;          // choice
-        return solve(n - 1, n, 3, mat);
+        dp.assign(n + 1, vector<int> (4, 0));
+        
+        dp[0][0] = max(mat[0][1], mat[0][2]);
+        dp[0][1] = max(mat[0][0], mat[0][2]);
+        dp[0][2] = max(mat[0][0], mat[0][1]);
+        dp[0][3] = max({mat[0][0], mat[0][1], mat[0][2]});
+        
+        for(int i = 1; i < n; i++) {
+            for(int last = 0; last < 4; last++) {
+                dp[i][last] = 0;
+                for(int j = 0; j < 3; j++) {
+                    if(j != last) {
+                        dp[i][last] = max(dp[i][last], dp[i - 1][j] + mat[i][j]);
+                    }
+                }
+            }
+        }
+        
+        return dp[n - 1][3];
+        
+        // return solve(n - 1, 3, mat);
     }
 };
