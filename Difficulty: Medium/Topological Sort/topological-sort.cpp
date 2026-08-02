918b5cfc4658e382a3af2465991e1052746e99f5
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-08-03 02:48:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,71 +1,33 @@
 class Solution {
   public:
-    void DFS(int node, vector<vector<int>>& adj, stack<int> &st, vector<bool> &visited) {
-        visited[node] = true;
-        
-        for(auto it : adj[node]) {
-            if(!visited[it]) {
-                DFS(it, adj, st, visited);
+    void dfs(int node, vector<vector<int>>& adj, vector<int> &vis, stack<int> &st) {
+        vis[node] = 1;
+        for(int nbr : adj[node]) {
+            if(!vis[nbr]) {
+                dfs(nbr, adj, vis, st);
             }
         }
         
         st.push(node);
     }
-    
-    
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        // 🔥 STEP 1: Build adjacency list
+        stack<int> st;
+        vector<int> vis(V, 0);
         vector<vector<int>> adj(V);
-        for (auto &e : edges) {
+        for(auto e : edges) {
             adj[e[0]].push_back(e[1]);
         }
         
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, adj, vis, st);
+            }
+        }
+        
         vector<int> ans;
-        // vector<bool> visited(V, false);
-        // stack<int> st;
-        
-        // for(int i = 0; i < V; i++) {
-        //     if(!visited[i]) {
-        //         DFS(i, adj, st, visited);
-        //     }
-        // }
-        
-        
-        
-        // while(!st.empty()) {
-        //     ans.push_back(st.top());
-        //     st.pop();
-        // }
-        
-        
-        // M2: Kahn's Algo.
-        
-        vector<int> inDeg(V, 0);
-        for(int i = 0; i < V; i++) {
-            for(int j = 0; j < adj[i].size(); j++) {
-                inDeg[adj[i][j]]++;
-            }
-        }
-        
-        // push all node of zero indegree into queue;
-        queue<int> q;
-        for(int i = 0; i < V; i++) {
-            if(!inDeg[i]) {
-                q.push(i);
-            }
-        }
-        
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            
-            for(int i = 0; i < adj[node].size(); i++) {
-                inDeg[adj[node][i]]--;
-                if(inDeg[adj[node][i]] == 0) {
-                    q.push(adj[node][i]);
-                }
-            }
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
         
         return ans;
