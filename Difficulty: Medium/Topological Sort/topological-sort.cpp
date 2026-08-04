542aedc9646b11f300f8cb912d9765e5fd55aa41
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-08-05 00:16:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,33 +1,36 @@
 class Solution {
   public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int> &vis, stack<int> &st) {
-        vis[node] = 1;
-        for(int nbr : adj[node]) {
-            if(!vis[nbr]) {
-                dfs(nbr, adj, vis, st);
-            }
-        }
-        
-        st.push(node);
-    }
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        stack<int> st;
-        vector<int> vis(V, 0);
         vector<vector<int>> adj(V);
         for(auto e : edges) {
             adj[e[0]].push_back(e[1]);
         }
         
+        vector<int> inDeg(V, 0);
         for(int i = 0; i < V; i++) {
-            if(!vis[i]) {
-                dfs(i, adj, vis, st);
+            for(auto it : adj[i]) {
+                inDeg[it]++;
             }
         }
         
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(inDeg[i] == 0) {
+                q.push(i);
+            }
+        }
         vector<int> ans;
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto nbr : adj[node]) {
+                inDeg[nbr]--;
+                if(inDeg[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
         }
         
         return ans;
