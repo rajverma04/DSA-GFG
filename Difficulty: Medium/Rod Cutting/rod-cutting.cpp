3476162ce323a246b04193f7313b36d591010eac
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-08-16 01:29:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -1,31 +1,30 @@
-// Unbounded Knapsack problem
-
 class Solution {
   public:
     vector<vector<int>> dp;
-    
-    int find(int index, int n, vector<int> &price) {
-        if(index == 0) {
-            return n * price[0];
+    int sol(int i, vector<int> &price, int N) {
+        if(i < 0) {
+            return 0;
         }
-        if(dp[index][n] != -1) {
-            return dp[index][n];
+        int rodlen = i + 1;     // 1 based idx
+        if(i == 0) {
+            if(N % rodlen == 0) {
+                return (N / rodlen) * price[0];
+            }
+            return 0;
         }
-        int notTake = 0 + find(index - 1, n, price);
-        
+        if(dp[i][N] != -1) {
+            return dp[i][N];
+        }
+        int notTake = sol(i - 1, price, N);
         int take = INT_MIN;
-        int rodLength = index + 1;
-    
-        if(rodLength <= n) {
-            take = price[index] + find(index, n - rodLength, price);
+        if(rodlen <= N) {
+            take = price[i] + sol(i, price, N - rodlen);
         }
-        
-        return dp[index][n] = max(take, notTake);
+        return dp[i][N] = max(take, notTake);
     }
     int cutRod(vector<int> &price) {
         int n = price.size();
         dp.assign(n, vector<int> (n + 1, -1));
-        
-        return find(n - 1, n, price);
+        return sol(n - 1, price, n);
     }
 };
