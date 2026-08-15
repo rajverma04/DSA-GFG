74a834589bd1e7b118fe60c40ebb43ccb5c4bca5
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-08-16 02:06:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -7,7 +7,7 @@ class Solution {
         }
         int rodlen = i + 1;     // 1 based idx
         if(i == 0) {
-            if(N % rodlen == 0) {
+            if(N % rodlen == 0) {       // rodlen = 1
                 return (N / rodlen) * price[0];
             }
             return 0;
@@ -24,7 +24,28 @@ class Solution {
     }
     int cutRod(vector<int> &price) {
         int n = price.size();
-        dp.assign(n, vector<int> (n + 1, -1));
-        return sol(n - 1, price, n);
+        dp.assign(n, vector<int> (n + 1, 0));
+        // return sol(n - 1, price, n);
+        int rodlen = 1;
+        // for(int N = 0; N <= n; N++) {
+        //     dp[0][N] = (N / rodlen) * price[0];
+        // }
+        vector<int> prev(n + 1, 0);
+        for(int N = 0; N <= n; N++) {
+            prev[N] = (N / rodlen) * price[0];
+        }
+        for(int i = 1; i < n; i++) {
+            rodlen = i + 1;
+            for(int N = 0; N <= n; N++) {
+                int notTake = prev[N];
+                int take = INT_MIN;
+                if(rodlen <= N) {
+                    take = price[i] + prev[N - rodlen];
+                }
+                prev[N] = max(take, notTake);
+            }
+        }
+        
+        return prev[n];
     }
 };
